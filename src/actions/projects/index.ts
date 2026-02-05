7d4f1878099c71ab19a5df198d41c231dbd7437f
Thomas Huber <th-huber@web.de>
2026-02-05 10:55:35 +0100

    feat: #12 add and remove managers to projects

--- src/actions/projects/index.ts
+++ src/actions/projects/index.ts
@@ -1,4 +1,5 @@
 export { addProjectManagerAction } from './mutations/add-project-manager';
+export { removeProjectManagerAction } from './mutations/remove-project-manager';
 export { createProjectAction } from './mutations/create-project';
 export { removeProjectManagerAction } from './mutations/remove-project-manager';
 export { getProjectUnsafe } from './queries/get-project';
