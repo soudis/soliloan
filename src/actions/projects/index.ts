612796152e4d0cc5d160402ff1ab8917f25a5909
Thomas Huber <th-huber@web.de>
2026-02-05 09:54:38 +0100

    feat: #12: (AI) add and remove managers to projects. Initial Code from cursor.

--- src/actions/projects/index.ts
+++ src/actions/projects/index.ts
@@ -1,3 +1,4 @@
+export { addProjectManagerAction } from './mutations/add-project-manager';
 export { createProjectAction } from './mutations/create-project';
 export { getProjectUnsafe } from './queries/get-project';
 export { getProjects } from './queries/get-projects';
