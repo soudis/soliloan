97454392f4c9cff92e490797da701afaabf105b0
Florian Humer <florian.humer@gmail.com>
2026-05-21 12:47:33 +0200

    fix: sort repaid loans last

--- src/lib/utils/sorters.ts
+++ src/lib/utils/sorters.ts
@@ -1,5 +1,7 @@
 import { type Transaction, TransactionType } from '@prisma/client';
 
+import { LoanStatus } from '@/types/loans';
+
 export const transactionSorter = (a: Transaction, b: Transaction) => {
   if (a.date > b.date) return 1;
   if (b.date > a.date) return -1;
@@ -10,7 +12,11 @@ export const transactionSorter = (a: Transaction, b: Transaction) => {
   return 0;
 };
 
-export const loansSorter = <T extends { signDate: Date }>(a: T, b: T) => {
-  const score = b.signDate.getTime() - a.signDate.getTime();
-  return score;
+export const loansSorter = <T extends { signDate: Date; status: LoanStatus | string }>(a: T, b: T) => {
+  const aRepaid = a.status === LoanStatus.REPAID;
+  const bRepaid = b.status === LoanStatus.REPAID;
+  if (aRepaid !== bRepaid) {
+    return aRepaid ? 1 : -1;
+  }
+  return b.signDate.getTime() - a.signDate.getTime();
 };
